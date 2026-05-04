287da1bb0a34619d88ec3be4d556876436a0c41b
Kurtaush <sahsaxxxz123@yandex.ru>
2026-05-04 11:01:20 +0900

    Проект

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting; 
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicsChart
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PaymentsBaseEntities _context = new PaymentsBaseEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChartPayments.ChartAreas.Add(new System.Windows.Forms.DataVisualization.Charting.ChartArea("Main"));
+
+            var currentSeries = new Series("Payments")
+            {
+                IsValueShownAsLabel = true,
+            };
+            ChartPayments.Series.Add(currentSeries);
+
+            ComboUsers.ItemsSource = _context.Users.ToList();
+            ComboChartTypes.ItemsSource = Enum.GetValues(typeof(SeriesChartType));  
+        }
+        private void UpdateChart(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboUsers.SelectedItem is Users currentUser && ComboChartTypes.SelectedItem is SeriesChartType currentType)
+            {
+                Series currentSeries = ChartPayments.Series.FirstOrDefault();
+                currentSeries.ChartType = currentType;
+                currentSeries.Points.Clear();
+
+                var categoriesList = _context.Categories.ToList();
+                foreach (var category in categoriesList)
+                {
+                    currentSeries.Points.AddXY(category.Name, _context.Payments.ToList().Where(p => p.Users == currentUser && p.Categories == category).Sum(p => p.Price * p.Num));
+                }
+            }
+        }
+    }
+}
